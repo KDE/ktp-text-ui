c3b3b1ba3955ad8a8986edd71a5877aa22064813
David Edmundson <kde@davidedmundson.co.uk>
2010-09-22 19:22:14 +0000

    Continued tidy of passing info from telepathy -> Adium themes. Added class AdiumThemeContentInfo, though this is not yet used.
    svn path=/trunk/playground/network/telepathy-chat-handler/; revision=1178339

---
+++ lib/adiumthemecontentinfo.cpp
@@ -0,0 +1,82 @@
+#include "adiumthemecontentinfo.h"
+#include <QString>
+
+class AdiumThemeContentInfoPrivate
+{
+public:
+    QString userIconPath;
+    QString senderScreenName;
+    QString sender;
+    QString senderColor;
+    QString senderStatusIcon;
+    QString senderDisplayName;
+    QString textbackgroundcolor;
+};
+
+AdiumThemeContentInfo::AdiumThemeContentInfo() :
+        d(new AdiumThemeContentInfoPrivate)
+{
+}
+
+QString AdiumThemeContentInfo::userIconPath() const
+{
+    return d->userIconPath;
+}
+
+void AdiumThemeContentInfo::setUserIconPath(const QString &userIconPath)
+{
+    d->userIconPath = userIconPath;
+}
+
+
+QString AdiumThemeContentInfo::senderScreenName() const
+{
+    return d->senderScreenName;
+}
+
+void AdiumThemeContentInfo::setSenderScreenName(const QString senderScreenName)
+{
+    d->senderScreenName = senderScreenName;
+}
+
+QString AdiumThemeContentInfo::sender() const
+{
+    return d->senderDisplayName;
+}
+
+
+QString AdiumThemeContentInfo::senderColor() const
+{
+    return d->senderColor;
+}
+
+void AdiumThemeContentInfo::setSenderColor(const QString &senderColor)
+{
+    d->senderColor = senderColor;
+}
+
+QString AdiumThemeContentInfo::senderStatusIcon() const
+{
+    return d->senderStatusIcon;
+}
+
+void AdiumThemeContentInfo::setSenderStatusIcon(const QString &senderStatusIcon)
+{
+    d->senderStatusIcon = senderStatusIcon;
+}
+
+QString AdiumThemeContentInfo::messageDirection() const
+{
+    //FIXME
+    return "rtl";
+}
+
+QString AdiumThemeContentInfo::senderDisplayName() const
+{
+    return d->senderDisplayName;
+}
+
+void AdiumThemeContentInfo::setSenderDisplayName(const QString &senderDisplayName)
+{
+    d->senderDisplayName = senderDisplayName;
+}
---
+++ lib/adiumthemecontentinfo.h
@@ -0,0 +1,46 @@
+#ifndef ADIUMTHEMECONTENTINFO_H
+#define ADIUMTHEMECONTENTINFO_H
+
+class QString;
+class AdiumThemeContentInfoPrivate;
+
+
+class AdiumThemeContentInfo
+{
+public:
+    explicit AdiumThemeContentInfo();
+
+    /** Path to the user icon associated with this message */
+    QString userIconPath() const;
+    void setUserIconPath(const QString& userIconPath);
+
+    /** The screen name (UID, ID, member name, etc.) of the sender of this message.*/
+    QString senderScreenName() const;
+    void setSenderScreenName(const QString senderScreenName);
+
+    /** The name of the sender of this message as the user's preferences request it.*/
+    QString sender() const;
+
+    /** A color derived from the user's name*/
+    //FIXME what is this talking about...?
+    QString senderColor() const;
+    void setSenderColor(const QString& senderColor);
+
+    /** The path to the status icon of the sender (available, away, etc...) */
+    QString senderStatusIcon() const;
+    void setSenderStatusIcon(const QString& senderStatusIcon);
+
+    /** The text direction of the message (either rtl or ltr) */
+    QString messageDirection() const;
+
+    /** The serverside (remotely set) name of the sender, such as an MSN display name.*/
+    QString senderDisplayName() const;
+    void setSenderDisplayName(const QString& senderDisplayName);
+
+    //textBackgroundColor
+
+private:
+    AdiumThemeContentInfoPrivate* d;
+};
+
+#endif // ADIUMTHEMECONTENTINFO_H
