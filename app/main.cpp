574a32e0a42c092c5a236913b108ba18bc5cdfe7
David Edmundson <kde@davidedmundson.co.uk>
2010-10-05 14:05:08 +0000

    Fake the application name of the chat handler to "ktelepathy" on load so that notifications will work.
    svn path=/trunk/playground/network/telepathy-chat-handler/; revision=1182740

--- app/main.cpp
+++ app/main.cpp
@@ -32,7 +32,7 @@
 int main(int argc, char *argv[])
 {
 
-    KAboutData aboutData("telepathy-chat-handler",
+    KAboutData aboutData("ktelepathy",
                          0,
                          ki18n("Telepathy Chat Handler"),
                          "0.1");
