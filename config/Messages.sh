e2eb69d16d356dd81034b31ea40b44bd939832ec
Albert Astals Cid <aacid@kde.org>
2011-02-12 13:41:30 +0000

    Proper syntax
    CCMAIL: dario.freddi@collabora.co.uk

--- config/Messages.sh
+++ config/Messages.sh
@@ -1,4 +1,4 @@
-! /usr/bin/env bash
+#! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp"` -o $podir/telepathy-chat-window-config.pot
 rm -f rc.cpp
