2492b7fd26772b29ff62bf58cfbc90a629ded1e3
Florian Reinhard <florian.reinhard@googlemail.com>
2011-03-14 23:20:03 +0100

    Rename telepathy-chat-handler to telepathy-kde-text-ui.

--- app/main.cpp
+++ app/main.cpp
@@ -32,7 +32,7 @@
 
 int main(int argc, char *argv[])
 {
-    KAboutData aboutData("telepathy-chat-handler",
+    KAboutData aboutData("telepathy-kde-text-ui",
                          0,
                          ki18n("Telepathy Chat Handler"),
                          "0.1");
