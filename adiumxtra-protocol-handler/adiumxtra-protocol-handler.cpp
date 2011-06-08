d7b108944030c8867e904b4a497eaf3c28285b7d
George Kiagiadakis <george.kiagiadakis@collabora.com>
2011-06-08 18:10:44 +0300

    Fix licenses to comply with the KDE licensing policy.
    REVIEW: 101555
    Acked-by: David Edmundson <kde@davidedmundson.co.uk>
    Acked-by: Dominik Schmidt <domme@rautelinux.org>

--- adiumxtra-protocol-handler/adiumxtra-protocol-handler.cpp
+++ adiumxtra-protocol-handler/adiumxtra-protocol-handler.cpp
@@ -1,10 +1,10 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) <year>  <name of author>
+    KDE Telepathy AdiumxtraProtocolHandler - Install Adiumxtra packages through adiumxtra://-pseudo protocol
+    Copyright (C) 2010 Dominik Schmidt <domme@rautelinux.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+    the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
@@ -15,7 +15,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 
 #include "adiumxtra-protocol-handler.h"
 #include "chat-style-installer.h"
