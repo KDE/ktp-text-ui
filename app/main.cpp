37bde897217a7195ddd81143e45ead72f12eaf46
David Edmundson <kde@davidedmundson.co.uk>
2011-09-16 14:54:04 +0100

    set new product name for bug reporting

--- app/main.cpp
+++ app/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
     aboutData.addAuthor(ki18n("David Edmundson"), ki18n("Developer"), "david@davidedmundson.co.uk");
     aboutData.addAuthor(ki18n("Dominik Schmidt"), ki18n("Developer"), "kde@dominik-schmidt.de");
     aboutData.addAuthor(ki18n("Francesco Nwokeka"), ki18n("Developer"), "francesco.nwokeka@gmail.com");
-    aboutData.setProductName("telepathy/text-chat-ui"); //set the correct name for bug reporting
+    aboutData.setProductName("telepathy/text-text-ui"); //set the correct name for bug reporting
 
     KCmdLineArgs::init(argc, argv, &aboutData);
 
