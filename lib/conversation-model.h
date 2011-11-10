a83582ed5426f01c5e50e6d53304cc493fee8ebf
Lasath Fernando <kde@lasath.org>
2011-11-10 17:42:50 +1100

    Created initial skelelton files for class ConversationModel

---
+++ lib/conversation-model.h
@@ -0,0 +1,36 @@
+/*
+    <one line to give the library's name and an idea of what it does.>
+    Copyright (C) 2011  Lasath Fernando <kde@lasath.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef CONVERSATIONMODEL_H
+#define CONVERSATIONMODEL_H
+
+#include <QAbstractItemModel>
+
+
+class ConversationModel : public QAbstractListModel
+{
+
+public:
+    explicit ConversationModel(QObject* parent = 0);
+    virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+    virtual int rowCount(const QModelIndex& parent = QModelIndex()) const;
+};
+
+#endif // CONVERSATIONMODEL_H
