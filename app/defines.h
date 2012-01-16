5dfed1cf2127e27901bd8c6571cb682674aa0e3a
Martin Klapetek <martin.klapetek@gmail.com>
2012-01-16 22:31:46 +0100

    Use correct string for text-ui client path
    BUG: 288327

--- app/defines.h
+++ app/defines.h
@@ -2,6 +2,6 @@
 #define DEFINES_H
 
 #define KTP_TEXTUI_CLIENT_NAME "KTp.TextUi"
-#define KTP_TEXTUI_CLIENT_PATH "org.freedesktop.Telepathy.KTp.TextUi"
+#define KTP_TEXTUI_CLIENT_PATH "org.freedesktop.Telepathy.Client.KTp.TextUi"
 
 #endif // DEFINES_H
