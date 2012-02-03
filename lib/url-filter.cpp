8abc04f50a37e44b46ba661674b646d5580cd74e
Lasath Fernando <kde@lasath.org>
2012-02-03 19:59:03 +1100

    Credit David for the code stolen from AdiumThemeView

--- lib/url-filter.cpp
+++ lib/url-filter.cpp
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2012  Lasath Fernando <kde@lasath.org>
+    Copyright (C) 2012  David Edmundson <kde@davidedmundson.co.uk>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
