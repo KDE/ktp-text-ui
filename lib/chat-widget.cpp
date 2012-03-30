KDE/ktp-text-ui

commit 533cdb2815c68b178e9942c349f09eba8df1f281

Remove empty lines and line-breaks from KNotify chat messages
Apply QString::simplified() on the chat message before
displaying it.

Reviewed-by: Martin Klapetek

lib/chat-widget.cpp
@@ -622,13 +622,16 @@ void ChatWidget::notifyAboutIncomingMessage(const Tp::ReceivedMessage & message)
         notification->setPixmap(notificationPixmap);
     }
 
-    if (message.text().length() > 170) {
+    // Remove empty lines from message
+    QString notifyText = msg.text().simplified();
+    if (notifyText.length() > 170) {
         //search for the closest space in text
-        QString truncatedMsg = message.text().left(message.text().indexOf(QLatin1Char(' '), 150)).append(QLatin1String("..."));
+        QString truncatedMsg = notifyText.left(notifyText.indexOf(QLatin1Char(' '), 150)).append(QLatin1String("..."));
         notification->setText(truncatedMsg);
     } else {
-        notification->setText(message.text());
+        notification->setText(notifyText);
     }
+
     //allows per contact notifications
     notification->addContext(QLatin1String("contact"), message.sender()->id());
     //TODO notification->addContext("group",... Requires KDE Telepathy Contact to work out which group they are in.

commit a378b0e959102f4ef492bc403753a3ce51e3afd3

Fix build broken by commit 533cdb2815c68b178e9942c349f09eba8df1f281

lib/chat-widget.cpp
@@ -623,7 +623,7 @@ void ChatWidget::notifyAboutIncomingMessage(const Tp::ReceivedMessage & message)
     }
 
     // Remove empty lines from message
-    QString notifyText = msg.text().simplified();
+    QString notifyText = message.text().simplified();
     if (notifyText.length() > 170) {
         //search for the closest space in text
         QString truncatedMsg = notifyText.left(notifyText.indexOf(QLatin1Char(' '), 150)).append(QLatin1String("..."));