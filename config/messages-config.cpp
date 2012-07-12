b6bc581f5b243f4bfa31ddb07faa9585fc8ea30a
Lasath Fernando <kde@lasath.org>
2012-07-12 23:16:24 +1000

    Implement skeleton KCM
    Currently it just contains a KPluginSelector. I'm comitting before
    I proceed any further, because I still haven't determined the best
    way of passing it its KConfigGroup.

---
+++ config/messages-config.cpp
@@ -0,0 +1,42 @@
+/*
+ *    Copyright (C) 2012  Lasath Fernando <kde@lasath.org>
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#include "messages-config.h"
+
+#include <KPluginFactory>
+#include <KPluginSelector>
+#include <QVBoxLayout>
+
+K_PLUGIN_FACTORY(KTpMessagesConfigFactory, registerPlugin<MessagesConfig>();)
+K_EXPORT_PLUGIN(KTpMessagesConfigFactory("kcm_ktp_message_filters", "kcm_ktp_chat_messages"))
+
+class MessagesConfig::Private {
+public:
+    KPluginSelector *selector;
+};
+
+MessagesConfig::MessagesConfig(QWidget *parent, const QVariantList &args)
+    : KCModule(KTpMessagesConfigFactory::componentData(), parent, args),
+        d(new Private)
+{
+    d->selector = new KPluginSelector();
+
+    QLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(d->selector);
+    setLayout(layout);
+}
