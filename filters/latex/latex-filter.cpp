92eea418191264a69fb3dae311592227e67c73c6
Lasath Fernando <kde@lasath.org>
2012-08-06 10:35:26 +1000

    Address issues of first review
    REVIEW:105887

--- filters/latex/latex-filter.cpp
+++ filters/latex/latex-filter.cpp
@@ -1,6 +1,10 @@
 /*
  *    Copyright (C) 2012  Lasath Fernando <kde@lasath.org>
  *
+ *    Copyright (c) 2004 by Duncan Mac-Vicar Prett   <duncan@kde.org>
+ *    Copyright (c) 2004-2005 by Olivier Goffart  <ogoffart@kde. org>
+ *    Kopete    (c) 2001-2004 by the Kopete developers  <kopete-devel@kde.org>
+ *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation; either
