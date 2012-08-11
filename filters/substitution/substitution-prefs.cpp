8361d985f86cbb577c79456812841a4083567b42
Lasath Fernando <kde@lasath.org>
2012-08-11 13:16:37 +1000

    Partially Implement SubstitutionPrefs
    So the filter now works with the default words hardcoded into the class.
    I don't know if these should be i18n'ed or removed completely.
    
    I'll implement the KCM side of this class after the KCM.

---
+++ filters/substitution/substitution-prefs.cpp
@@ -0,0 +1,72 @@
+/*
+ *    Copyright (C) 2012  Lasath Fernando <kde@lasath.org>
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#include "substitution-prefs.h"
+
+#include <KSharedConfig>
+#include <KGlobal>
+
+class SubstitutionPrefs::Private {
+public:
+     SubstitutionPrefs::List wordList;
+};
+
+SubstitutionPrefs::SubstitutionPrefs()
+    : d(new Private)
+{
+    load();
+}
+
+SubstitutionPrefs::~SubstitutionPrefs()
+{
+    delete d;
+}
+
+void SubstitutionPrefs::load()
+{
+    KSharedConfig::Ptr config = KGlobal::config();
+    d->wordList = config->entryMap(QLatin1String("replacements"));
+
+    if (d->wordList.isEmpty()) {
+        d->wordList = defaultList();
+    }
+}
+
+SubstitutionPrefs::List SubstitutionPrefs::defaultList()
+{
+    List def;
+
+    def[QLatin1String("(TM) ")] = QLatin1String("™ ");
+    def[QLatin1String(" uber ")] = QLatin1String(" über ");
+
+    //probably not particularly useful without i18n
+    def[QLatin1String(" dnt ")] = QLatin1String(" don't ");
+    def[QLatin1String(" cnt ")] = QLatin1String(" can't ");
+
+    return def;
+}
+
+QString SubstitutionPrefs::replacementFor(const QString &word)
+{
+    return d->wordList[word];
+}
+
+QStringList SubstitutionPrefs::wordsToReplace()
+{
+    return d->wordList.keys();
+}
--- filters/substitution/substitution-prefs.h
+++ filters/substitution/substitution-prefs.h
@@ -16,38 +16,31 @@
  *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
-#include "substitution-filter.h"
-#include "substitution-prefs.h"
+#ifndef SUBSTITUTION_PREFS_H
+#define SUBSTITUTION_PREFS_H
 
-#include <KPluginFactory>
-#include <KDebug>
-#include <KUrl>
-#include <KUriFilter>
+#include <QMap>
+#include <QStringList>
 
-class SubstitutionFilter::Private
+class SubstitutionPrefs
 {
+
 public:
-    SubstitutionPrefs prefs;
+    SubstitutionPrefs();
+    virtual ~SubstitutionPrefs();
+
+    QString replacementFor(const QString &word);
+    QStringList wordsToReplace();
+
+    void load();
+    void save();
+
+    typedef QMap<QString, QString> List;
+    List defaultList();
+
+private:
+    class Private;
+    Private *d;
 };
 
-SubstitutionFilter::SubstitutionFilter(QObject *parent, const QVariantList &) :
-    AbstractMessageFilter(parent), d(new Private)
-{
-}
-
-SubstitutionFilter::~SubstitutionFilter()
-{
-    delete d;
-}
-
-void SubstitutionFilter::filterMessage(Message &message)
-{
-    QString msg = message.mainMessagePart();
-    Q_FOREACH (const QString &word, d->prefs.wordsToReplace()) {
-        msg.replace(word, d->prefs.replacementFor(word));
-    }
-    message.setMainMessagePart(msg);
-}
-
-K_PLUGIN_FACTORY(MessageFilterFactory, registerPlugin<SubstitutionFilter>();)
-K_EXPORT_PLUGIN(MessageFilterFactory("ktptextui_message_filter_substitution"))
+#endif // SUBSTITUTION_PREFS_H
