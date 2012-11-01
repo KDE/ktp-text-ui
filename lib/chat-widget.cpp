KDE/ktp-text-ui

commit b15541ff10ad46e7386231b133bb5ef195c1612c

Use initialization list

lib/chat-widget.cpp
@@ -55,10 +55,10 @@
 class ChatWidgetPrivate
 {
 public:
-    ChatWidgetPrivate()
+    ChatWidgetPrivate() :
+        remoteContactChatState(Tp::ChannelChatStateInactive);
+        isGroupChat(false)
     {
-        isGroupChat = false;
-        remoteContactChatState = Tp::ChannelChatStateInactive;
     }
     /** Stores whether the channel is ready with all contacts upgraded*/
     bool chatviewlInitialised;

commit 9595d7201de52308132ffd818c265a133dd2906f

Fix build
Fix typo introduced in b15541ff1

lib/chat-widget.cpp
@@ -56,7 +56,7 @@ class ChatWidgetPrivate
 {
 public:
     ChatWidgetPrivate() :
-        remoteContactChatState(Tp::ChannelChatStateInactive);
+        remoteContactChatState(Tp::ChannelChatStateInactive),
         isGroupChat(false)
     {
     }