8e176284f8c15b451a92f7026b03d566d5d89780
Lasath Fernando <kde@lasath.org>
2013-02-10 01:54:12 +0000

    Add a new filter that turns messages containg bugzilla bug URL's into displaying the full title + status

---
+++ filters/bugzilla/bugzilla-filter.cpp
@@ -0,0 +1,115 @@
+/*
+ *    Copyright (C) 2012  Lasath Fernando <kde@lasath.org>
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#include "bugzilla-filter.h"
+
+#include <qjson/parser.h>
+
+#include <KPluginFactory>
+#include <KDebug>
+#include <KUrl>
+#include <KUriFilter>
+#include <KIO/Job>
+#include <QTextDocument>
+
+class BugzillaFilter::Private
+{
+public:
+    QRegExp bugText;
+    QString sectionTemplate;
+};
+
+BugzillaFilter::BugzillaFilter(QObject *parent, const QVariantList &) :
+    AbstractMessageFilter(parent), d(new Private)
+{
+    d->bugText = QRegExp(QLatin1String("BUG:[ ]*(\\d+)"));
+    d->sectionTemplate = QLatin1String("[BUG <a href='%1'>%2</a>] %3");
+}
+
+BugzillaFilter::~BugzillaFilter()
+{
+    delete d;
+}
+
+void BugzillaFilter::addBugDescription(Message &msg, KUrl &baseUrl) {
+    KUrl request;
+    request.setHost(baseUrl.host());
+    request.setProtocol(baseUrl.protocol());
+    request.setDirectory(baseUrl.directory());
+    request.setFileName(QLatin1String("jsonrpc.cgi"));
+
+    request.addQueryItem(QLatin1String("method"), QLatin1String("Bug.get"));
+    request.addQueryItem(QLatin1String("params"),
+                         QString(QLatin1String("[{\"ids\":[%1]}]")).
+                         arg(baseUrl.queryItemValue(QLatin1String("id"))));
+
+    kDebug() << request;
+    KIO::StoredTransferJob *job = KIO::storedGet(request);
+    job->exec();
+
+    QVariantMap response = QJson::Parser().parse(job->data()).toMap();
+    if (response.contains(QLatin1String("result"))) {
+        QVariantMap result = response.value(QLatin1String("result")).toMap();
+        if (result.contains(QLatin1String("bugs"))) {
+            QVariantList bugs = result.value(QLatin1String("bugs")).toList();
+            if (!bugs.isEmpty()) {
+                QVariantMap bug = bugs.first().toMap();
+                if (bug.contains(QLatin1String("summary"))) {
+                    QString summary = bug.value(QLatin1String("summary")).toString();
+                    kDebug() << summary;
+
+                    msg.appendMessagePart(d->sectionTemplate
+                        .arg(baseUrl.url())
+                        .arg(Qt::escape(baseUrl.queryItemValue(QLatin1String("id"))))
+                        .arg(Qt::escape(summary)));
+                }
+            }
+        }
+    }
+
+}
+
+void BugzillaFilter::filterMessage(Message &message)
+{
+    QString msg = message.mainMessagePart();
+    int index = msg.indexOf(d->bugText);
+    while (index >= 0) {
+        KUrl baseUrl;
+        baseUrl.setProtocol(QLatin1String("https"));
+        baseUrl.setHost(QLatin1String("bugs.kde.org"));
+        baseUrl.setFileName(QLatin1String("show_bug.cgi"));
+        baseUrl.addQueryItem(QLatin1String("id"), d->bugText.cap(1));
+
+        addBugDescription(message, baseUrl);
+        index = msg.indexOf(d->bugText, index + 1);
+    }
+
+    Q_FOREACH (QVariant var, message.property("Urls").toList()) {
+        KUrl url = qvariant_cast<KUrl>(var);
+
+        if (url.fileName() == QLatin1String("show_bug.cgi")) { //a bugzilla of some sort
+            kDebug() << "link is to a bugzilla:" << url.host();
+
+            //by using the host from the link, it will work with *any* bugzilla installation
+            addBugDescription(message, url);
+        }
+    }
+}
+
+K_PLUGIN_FACTORY(MessageFilterFactory, registerPlugin<BugzillaFilter>();)
+K_EXPORT_PLUGIN(MessageFilterFactory("ktptextui_message_filter_bugzilla"))
