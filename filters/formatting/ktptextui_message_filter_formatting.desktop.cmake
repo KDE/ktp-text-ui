d6d90f45737af05eae0c6f4d38d8554bdee8e37d
Script Kiddy <scripty@kde.org>
2013-03-07 08:44:42 +0100

    SVN_SILENT made messages (.desktop file)

--- filters/formatting/ktptextui_message_filter_formatting.desktop.cmake
+++ filters/formatting/ktptextui_message_filter_formatting.desktop.cmake
@@ -1,7 +1,24 @@
 [Desktop Entry]
 Encoding=UTF-8
 Name=Text Formatting
+Name[cs]=Formátování textu
+Name[fi]=Tekstin muotoilu
+Name[ia]=Dar forma a texto
+Name[it]=Formattazione del testo
+Name[pt]=Formatação do Texto
+Name[sk]=Formátovanie textu
+Name[sl]=Oblikovanje besedila
+Name[uk]=Форматування тексту
+Name[x-test]=xxText Formattingxx
 Comment=Use /, _, * and - to make text italic, underlined, bold or strikeout respectively
+Comment[cs]=Použít /,_, * a - pro kurzívu, podtržený, tučný a přeškrtnutý text
+Comment[ia]=Usa /,_,* e - pro facer texto cursive, sublineate, grasse o strikeout respectivemente
+Comment[it]=Usa /, _, * e - per rendere il testo rispettivamente corsivo, sottolineato, grassetto o barrato
+Comment[pt]=Usar o '/', '_', '*' e '-' para tornar o texto itálico, sublinhado, negrito ou traçado, respectivamente
+Comment[sk]=Použite /,  _, * a - na zmenu textu na kurzívu, podčiarknuté, tučné alebo prečiarknuté
+Comment[sl]=Uporabite /,_, * in -, da bo besedilo ležeče, podčrtano, krepko ali prečrtano
+Comment[uk]=Використовувати /, _, * і - для курсиву, підкресленого, напівжирного та перекресленого тексту, відповідно
+Comment[x-test]=xxUse /, _, * and - to make text italic, underlined, bold or strikeout respectivelyxx
 Type=Service
 ServiceTypes=KTpTextUi/MessageFilter
 X-KDE-Library=ktptextui_message_filter_formatting
