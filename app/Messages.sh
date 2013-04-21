b37ae14cb260fd1ca563302050f3953a9b417aea
David Edmundson <kde@davidedmundson.co.uk>
2013-04-21 10:58:47 +0000

    Fix .rc file extraction in Messages.sh

--- app/Messages.sh
+++ app/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-#$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name "*.ui" -name "*.rc"` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp"` -o $podir/ktp-text-ui.pot
 #rm -f rc.cpp
