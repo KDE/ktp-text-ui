8ccc35f38af1d910b8e04f5a820167d184724713
Anant Kamath <kamathanant@gmail.com>
2013-05-02 16:13:50 +0530

    Added plugin to expand URL Shortener urls REVIEW: 110255

---
+++ filters/urlexpansion/ktptextui_message_filter_urlexpansion.desktop.cmake
@@ -0,0 +1,15 @@
+[Desktop Entry]
+Encoding=UTF-8
+Name=Short URLs
+Comment=Shows the actual URL to which a shortened URL is redirecting to
+Type=Service
+ServiceTypes=KTpTextUi/MessageFilter
+X-KDE-Library=ktptextui_message_filter_urlexpansion
+X-KDE-PluginInfo-Author=Anant Kamath
+X-KDE-PluginInfo-Email=kamathanant@gmail.org
+X-KDE-PluginInfo-Name=urlexpansion
+X-KDE-PluginInfo-Version=@KTP_TEXT_UI_VERSION@
+X-KDE-PluginInfo-Website=http://community.kde.org/KTp
+X-KDE-PluginInfo-License=LGPL
+X-KDE-PluginInfo-EnabledByDefault=true
+X-KTp-PluginInfo-Version=@KTP_MESSAGE_FILTER_FRAMEWORK_VERSION@
---
+++ filters/urlexpansion/urlexpansion-filter.cpp
@@ -0,0 +1,94 @@
+/*
+ *    Copyright (C) 2013  Anant Kamath <kamathanant@gmail.com>
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#include "urlexpansion-filter.h"
+
+#include<QFile>
+
+#include <KPluginFactory>
+#include <KDE/KStandardDirs>
+
+#include <qjson/parser.h>
+
+class UrlExpansionFilter::Private
+{
+public:
+    Private() {
+        requestCounter = 0;
+    }
+    int requestCounter;
+    QStringList supportedServices;
+};
+
+UrlExpansionFilter::UrlExpansionFilter(QObject *parent, const QVariantList &) :
+    AbstractMessageFilter(parent), d(new Private)
+{
+    getSupportedServices();
+}
+
+UrlExpansionFilter::~UrlExpansionFilter()
+{
+    delete d;
+}
+
+void UrlExpansionFilter::getSupportedServices()
+{
+    QFile servicesFile(KGlobal::dirs()->findResource("data", QLatin1String("ktelepathy/longurlServices.json")));
+    servicesFile.open(QIODevice::ReadOnly);
+    QVariantMap response = QJson::Parser().parse(&servicesFile).toMap();
+    d->supportedServices = response.uniqueKeys();
+}
+
+void UrlExpansionFilter::addExpandedUrl(KTp::Message &message, const KUrl &url)
+{
+    d->requestCounter++;
+    QString urlId = QString((QLatin1String("url") + QString::number(d->requestCounter)));
+    QString callbackFunction = QString((QLatin1String("expandUrlCallbacks.") + urlId));
+    KUrl request = KUrl(QLatin1String("http://api.longurl.org/v2/expand"));
+    request.addQueryItem(QLatin1String("url"), url.url());
+    request.addQueryItem(QLatin1String("format"), QLatin1String("json"));
+    request.addQueryItem(QLatin1String("callback"), callbackFunction);
+    request.addQueryItem(QLatin1String("user-agent"), QLatin1String("KTp"));
+
+    message.appendMessagePart(QString::fromLatin1("<p id = \"%1\">Redirects to </p>").arg(urlId));
+    message.appendScript(QString::fromLatin1("showShortUrl(\"%1\",\"%2\");").arg(request.prettyUrl(),urlId));
+}
+
+void UrlExpansionFilter::filterMessage(KTp::Message &message, const KTp::MessageContext &context)
+{
+    //if we're hidden we don't want to make network requests that can show we're online
+    if (context.account()->currentPresence().type() == Tp::ConnectionPresenceTypeHidden) {
+        return;
+    }
+
+    Q_FOREACH (const QVariant &var, message.property("Urls").toList()) {
+        KUrl url = qvariant_cast<KUrl>(var);
+
+        if (!url.path().isEmpty() && QString::compare(url.path(),QLatin1String("/")) && d->supportedServices.contains(url.host())) {
+            addExpandedUrl(message, url);
+        }
+    }
+}
+
+QStringList UrlExpansionFilter::requiredScripts()
+{
+    return QStringList() << QLatin1String("ktelepathy/longurl.js");
+}
+
+K_PLUGIN_FACTORY(MessageFilterFactory, registerPlugin<UrlExpansionFilter>();)
+K_EXPORT_PLUGIN(MessageFilterFactory("ktptextui_message_filter_urlexpansion"))
