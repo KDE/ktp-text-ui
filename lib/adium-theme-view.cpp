dd8ea96d3e9bf3dc1534c45bb433c56df36b8cf1
Sven Brauch <svenbrauch@googlemail.com>
2013-05-08 20:59:06 +0200

    Use a QWebPage instead of a KWebPage in the text UI.
    According to my tests, this fixes bug 318549, although
    I have no idea why it does. But since using QWebPage is
    preferable to KWebPage anyways (we don't use anything from
    KWebPage), the change makes sense even without that.
    CCBUG:318549

--- lib/adium-theme-view.cpp
+++ lib/adium-theme-view.cpp
@@ -51,7 +51,7 @@
 #include <KProtocolInfo>
 
 AdiumThemeView::AdiumThemeView(QWidget *parent)
-        : KWebView(parent),
+        : KWebView(parent, false),
         // check iconPath docs for minus sign in -KIconLoader::SizeLarge
         m_defaultAvatar(KIconLoader::global()->iconPath(QLatin1String("im-user"),-KIconLoader::SizeLarge)),
         m_displayHeader(true)
