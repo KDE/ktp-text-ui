KDE/ktp-text-ui

commit 62bb42b610fc4d9228ed77a61dbef143658d8340

Apply theme changes immediately
(text-ui now depends on TelepathyLoggerQt4 0.7.95/0.8.0)

BUG: 270350
REVIEW: 110545
FIXED-IN: 0.7.0

lib/chat-widget.cpp
@@ -52,6 +52,8 @@
 #include <TelepathyQt/PendingChannelRequest>
 #include <TelepathyQt/OutgoingFileTransferChannel>
 #include <TelepathyLoggerQt4/TextEvent>
+#include <TelepathyLoggerQt4/LogWalker>
+#include <TelepathyLoggerQt4/PendingEvents>
 
 #include <KTp/presence.h>
 #include <KTp/actions.h>
@@ -65,7 +67,8 @@ public:
     ChatWidgetPrivate() :
         remoteContactChatState(Tp::ChannelChatStateInactive),
         isGroupChat(false),
-        logsLoaded(false)
+        logsLoaded(false),
+        exchangedMessagesCount(0)
     {
     }
     /** Stores whether the channel is ready with all contacts upgraded*/
@@ -82,6 +85,7 @@ public:
     LogManager *logManager;
     QTimer *pausedStateTimer;
     bool logsLoaded;
+    uint exchangedMessagesCount;
 
     QList< Tp::OutgoingFileTransferChannelPtr > tmpFileTransfers;
 
@@ -173,7 +177,7 @@ ChatWidget::ChatWidget(const Tp::TextChannelPtr & channel, const Tp::AccountPtr 
     if (!d->isGroupChat) {
         KConfig config(QLatin1String("ktelepathyrc"));
         KConfigGroup tabConfig = config.group("Behavior");
-        d->logManager->setFetchAmount(tabConfig.readEntry<int>("scrollbackLength", 4));
+        d->logManager->setScrollbackLength(tabConfig.readEntry<int>("scrollbackLength", 4));
         d->logManager->setTextChannel(d->account, d->channel);
         m_previousConversationAvailable = d->logManager->exists();
     } else {
@@ -521,6 +525,8 @@ void ChatWidget::handleIncomingMessage(const Tp::ReceivedMessage &message, bool 
 
     if (d->chatViewInitialized) {
 
+        d->exchangedMessagesCount++;
+
         //debug the message parts (looking for HTML etc)
 //        Q_FOREACH(Tp::MessagePart part, message.parts())
 //        {
@@ -637,12 +643,17 @@ void ChatWidget::handleMessageSent(const Tp::Message &message, Tp::MessageSendin
     d->notifyFilter->filterMessage(processedMessage,
                                    KTp::MessageContext(d->account, d->channel));
     d->ui.chatArea->addMessage(processedMessage);
+    d->exchangedMessagesCount++;
 }
 
 void ChatWidget::chatViewReady()
 {
-    if (!d->logsLoaded) {
-        d->logManager->fetchLast();
+    if (!d->logsLoaded || d->exchangedMessagesCount > 0) {
+        if (d->exchangedMessagesCount == 0) {
+            d->logManager->fetchScrollback();
+        } else {
+            d->logManager->fetchHistory(d->exchangedMessagesCount + d->logManager->scrollbackLength());
+        }
     }
 
     d->logsLoaded = true;
@@ -991,4 +1002,13 @@ void ChatWidget::addEmoticonToChat(const QString &emoticon)
     d->ui.sendMessageBox->insertPlainText(QLatin1String(" ") + emoticon);
     d->ui.sendMessageBox->setFocus();
 }
+
+void ChatWidget::reloadTheme()
+{
+    d->logsLoaded = false;
+    d->chatViewInitialized = false;
+
+    initChatArea();
+}
+
 #include "chat-widget.moc"

CMakeLists.txt
@@ -15,7 +15,7 @@ set(KDE_MIN_VERSION "4.4.75")
 find_package (KDE4 ${KDE_MIN_VERSION} REQUIRED)
 find_package (TelepathyQt4 0.9.1 REQUIRED)
 find_package (KTp REQUIRED)
-find_package (TelepathyLoggerQt4 0.5.60 REQUIRED)
+find_package (TelepathyLoggerQt4 0.7.95 REQUIRED)
 
 
 # FUTURE: When we can depend on KDE 4.10, use emoticoncontexteditaction from here

app/chat-window.cpp
@@ -46,6 +46,7 @@
 #include <KMenu>
 #include <KToolBar>
 #include <KToolInvocation>
+#include <KCModuleProxy>
 
 #include <QEvent>
 #include <QWidgetAction>
@@ -591,7 +592,13 @@ void ChatWindow::showSettingsDialog()
 
     KSettings::Dialog *dialog = new KSettings::Dialog(this);
 
-    dialog->addModule(QLatin1String("kcm_ktp_chat_appearance"));
+    KPageWidgetItem *configPage = dialog->addModule(QLatin1String("kcm_ktp_chat_appearance"));
+    KCModuleProxy *proxy = qobject_cast<KCModuleProxy*>(configPage->widget());
+    Q_ASSERT(proxy);
+
+    connect(proxy->realModule(), SIGNAL(reloadTheme()),
+            this, SLOT(onReloadTheme()));
+
     dialog->addModule(QLatin1String("kcm_ktp_chat_behavior"));
     dialog->addModule(QLatin1String("kcm_ktp_chat_messages"));
 
@@ -956,6 +963,14 @@ void ChatWindow::updateSendMessageShortcuts()
     for (int i = 0; i < m_tabWidget->count(); i++) {
         ChatTab* tab = qobject_cast<ChatTab*>(m_tabWidget->widget(i));
         tab->updateSendMessageShortcuts(newSendMessageShortcuts);
+    }
+}
+
+void ChatWindow::onReloadTheme()
+{
+    for (int i = 0; i < m_tabWidget->count(); i++) {
+        ChatTab *tab = qobject_cast<ChatTab*>(m_tabWidget->widget(i));
+        tab->reloadTheme();
     }
 }
 

app/chat-window.h
@@ -109,6 +109,7 @@ private Q_SLOTS:
     void onZoomOut();
     void onZoomFactorChanged(qreal zoom);
     void updateSendMessageShortcuts();
+    void onReloadTheme();
 
 protected Q_SLOTS:
     void showSettingsDialog();

config/appearance-config.cpp
@@ -85,4 +85,6 @@ void AppearanceConfig::save()
     config->sync();
 
     KCModule::save();
+
+    Q_EMIT reloadTheme();
 }

config/appearance-config.h
@@ -39,6 +39,9 @@ public Q_SLOTS:
     virtual void load();
     virtual void defaults();
 
+Q_SIGNALS:
+    void reloadTheme();
+
 protected:
     AppearanceConfigTab *m_singleTab;
     AppearanceConfigTab *m_groupTab;

lib/chat-widget.h
@@ -113,6 +113,8 @@ public Q_SLOTS:
 
     void updateSendMessageShortcuts(const KShortcut &shortcuts);
 
+    void reloadTheme();
+
 protected:
     void changeEvent(QEvent *e);
     void resizeEvent(QResizeEvent *);

lib/logmanager.cpp
@@ -26,12 +26,11 @@
 #include <KDebug>
 
 #include <TelepathyLoggerQt4/Init>
+#include <TelepathyLoggerQt4/LogWalker>
 #include <TelepathyLoggerQt4/Entity>
-#include <TelepathyLoggerQt4/PendingDates>
 #include <TelepathyLoggerQt4/PendingEvents>
 #include <TelepathyLoggerQt4/Event>
 #include <TelepathyLoggerQt4/TextEvent>
-#include <TelepathyLoggerQt4/CallEvent>
 #include <TelepathyLoggerQt4/LogManager>
 
 #include <TelepathyQt/Types>
@@ -41,7 +40,7 @@
 
 LogManager::LogManager(QObject *parent)
     : QObject(parent),
-    m_fetchAmount(10)
+    m_scrolbackLength(10)
 {
     Tpl::init();
 
@@ -77,52 +76,42 @@ void LogManager::setTextChannel(const Tp::AccountPtr &account, const Tp::TextCha
     m_account = account;
 }
 
-void LogManager::setFetchAmount(int n)
+void LogManager::setScrollbackLength(int n)
 {
-    m_fetchAmount = n;
+    m_scrolbackLength = n;
 }
 
-void LogManager::fetchLast()
+int LogManager::scrollbackLength() const
 {
-    kDebug();
-    if (m_fetchAmount > 0 && !m_account.isNull() && !m_textChannel.isNull() && m_textChannel->targetHandleType() == Tp::HandleTypeContact) {
+    return m_scrolbackLength;
+}
+
+void LogManager::fetchScrollback()
+{
+    fetchHistory(m_scrolbackLength);
+}
+
+void LogManager::fetchHistory(int n)
+{
+    // Skip if no messages are requested
+    if (n > 0 && !m_account.isNull() && !m_textChannel.isNull()
+        && m_textChannel->targetHandleType() == Tp::HandleTypeContact) {
         Tpl::EntityPtr contactEntity = Tpl::Entity::create(m_textChannel->targetContact()->id().toLatin1().data(),
                                                 Tpl::EntityTypeContact,
                                                 NULL,
                                                 NULL);
 
-        Tpl::PendingDates* dates = m_logManager->queryDates(m_account, contactEntity, Tpl::EventTypeMaskText);
-        connect(dates, SIGNAL(finished(Tpl::PendingOperation*)), SLOT(onDatesFinished(Tpl::PendingOperation*)));
+        Tpl::LogWalkerPtr walker = m_logManager->queryWalkFilteredEvents(
+            m_account, contactEntity, Tpl::EventTypeMaskText, 0, 0);
+        Tpl::PendingEvents *events = walker->queryEvents(n);
+        connect(events, SIGNAL(finished(Tpl::PendingOperation*)),
+                this, SLOT(onEventsFinished(Tpl::PendingOperation*)));
         return;
     }
 
     //in all other cases finish immediately.
     QList<KTp::Message> messages;
     Q_EMIT fetched(messages);
-}
-
-void LogManager::onDatesFinished(Tpl::PendingOperation *po)
-{
-    Tpl::PendingDates *pd = (Tpl::PendingDates*) po;
-
-    if (pd->isError()) {
-        qWarning() << "error in PendingDates:" << pd->errorMessage();
-        return;
-    }
-
-    QList<QDate> dates = pd->dates();
-
-    if (!dates.isEmpty()) {
-        QDate date = dates.last();
-
-        kDebug() << pd->account()->uniqueIdentifier() << pd->entity()->identifier() << dates;
-
-        Tpl::PendingEvents *events = m_logManager->queryEvents( pd->account(), pd->entity(), Tpl::EventTypeMaskAny, date);
-        connect(events, SIGNAL(finished(Tpl::PendingOperation*)), SLOT(onEventsFinished(Tpl::PendingOperation*)));
-    } else {
-        QList<KTp::Message> messages;
-        Q_EMIT fetched(messages);
-    }
 }
 
 bool operator<(const Tpl::EventPtr &e1, const Tpl::EventPtr &e2)
@@ -155,21 +144,15 @@ void LogManager::onEventsFinished(Tpl::PendingOperation *po)
     // Uses the operator< overload above
     qSort(allEvents);
 
-    QList<Tpl::TextEventPtr> events;
-    QList<Tpl::EventPtr>::iterator i = allEvents.end();
-    while (i-- != allEvents.begin() && (events.count() < m_fetchAmount)) {
-        Tpl::TextEventPtr textEvent = (*i).dynamicCast<Tpl::TextEvent>();
+    QList<KTp::Message> messages;
+    Q_FOREACH (const Tpl::EventPtr &event, allEvents) {
+        const Tpl::TextEventPtr textEvent = event.dynamicCast<Tpl::TextEvent>();
         if (!textEvent.isNull()) {
             if (!queuedMessageTokens.contains(textEvent->messageToken())) {
-                events.prepend(textEvent);
+                const KTp::Message message = KTp::MessageProcessor::instance()->processIncomingMessage(textEvent, m_account, m_textChannel);
+                messages.append(message);
             }
         }
-    }
-
-    QList<KTp::Message> messages;
-    Q_FOREACH(const Tpl::TextEventPtr &event, events) {
-        KTp::Message message = KTp::MessageProcessor::instance()->processIncomingMessage(event, m_account, m_textChannel);
-        messages.append(message);
     }
 
     kDebug() << "emit all messages" << messages.count();

commit 770624e69b2ebfea5867ae1bbd6227de0597b4d0

Only listen to loadFinished() when initializing theme
Otherwise when a nested <iframe> from youtube plugin finishes
loading, we end up in an infinite loop of reloading content

Fixes regression introduced by 62bb42b610fc4d9228ed77a61dbef143658d8340

BUG: 320221
FIXED-IN: 0.7.0
Reviewed-By: David Edmundson

lib/chat-widget.cpp
@@ -147,7 +147,6 @@ ChatWidget::ChatWidget(const Tp::TextChannelPtr & channel, const Tp::AccountPtr 
     setAcceptDrops(true);
 
     /* Prepare the chat area */
-    connect(d->ui.chatArea, SIGNAL(loadFinished(bool)), SLOT(chatViewReady()), Qt::QueuedConnection);
     connect(d->ui.chatArea, SIGNAL(zoomFactorChanged(qreal)), SIGNAL(zoomFactorChanged(qreal)));
     connect(d->ui.chatArea, SIGNAL(textPasted()), d->ui.sendMessageBox, SLOT(pasteSelection()));
     initChatArea();
@@ -648,6 +647,8 @@ void ChatWidget::handleMessageSent(const Tp::Message &message, Tp::MessageSendin
 
 void ChatWidget::chatViewReady()
 {
+    disconnect(d->ui.chatArea, SIGNAL(loadFinished(bool)), this, SLOT(chatViewReady()));
+
     if (!d->logsLoaded || d->exchangedMessagesCount > 0) {
         if (d->exchangedMessagesCount == 0) {
             d->logManager->fetchScrollback();
@@ -940,6 +941,8 @@ qreal ChatWidget::zoomFactor() const
 
 void ChatWidget::initChatArea()
 {
+    connect(d->ui.chatArea, SIGNAL(loadFinished(bool)), SLOT(chatViewReady()), Qt::QueuedConnection);
+
     d->ui.chatArea->load((d->isGroupChat ? AdiumThemeView::GroupChat : AdiumThemeView::SingleUserChat));
 
     AdiumThemeHeaderInfo info;