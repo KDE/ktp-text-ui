2a64e8fafdacbf3ffbc6b01fad6c6e880a3029ff
Yuri Chornoivan <yurchor@ukr.net>
2014-08-20 12:14:57 +0300

    Extract all messages

--- logviewer/Messages.sh
+++ logviewer/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.rc"` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name "*.rc"` `find . -name "*.ui"`>> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp"` -o $podir/ktp-log-viewer.pot
 #rm -f rc.cpp
