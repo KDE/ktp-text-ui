d494687b02c83193aa78b96cd42436c074ee3930
Martin Klapetek <mklapetek@kde.org>
2015-01-07 17:02:17 +0100

    [image-sharer] Change the export macro to not use kdemacros.h

--- image-sharer/imagesharer_export.h
+++ image-sharer/imagesharer_export.h
@@ -1,21 +1,37 @@
+/*  This file is part of the KDE project
+ *    Copyright (C) 2007 David Faure <faure@kde.org>
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Library General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Library General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Library General Public License
+ *    along with this library; see the file COPYING.LIB.  If not, write to
+ *    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *    Boston, MA 02110-1301, USA.
+ */
+
 #ifndef IMAGESHARER_EXPORT_H
 #define IMAGESHARER_EXPORT_H
 
-// needed for KDE_EXPORT and KDE_IMPORT macros
-#include <kdemacros.h>
+#include <QtCore/QtGlobal>
 
 #ifndef IMAGESHARER_EXPORT
-# if defined(MAKE_IMAGESHARER_LIB)
-// We are building this library
-#  define IMAGESHARER_EXPORT KDE_EXPORT
+/* We are building this library */
+#  define IMAGESHARER_EXPORT __attribute__((visibility("default")))
 # else
-// We are using this library
-#  define IMAGESHARER_EXPORT KDE_IMPORT
-# endif
+/* We are using this library */
+#  define IMAGESHARER_EXPORT __attribute__((visibility("default")))
 #endif
 
 # ifndef IMAGESHARER_EXPORT_DEPRECATED
-#  define IMAGESHARER_EXPORT_DEPRECATED KDE_DEPRECATED IMAGESHARER_EXPORT
+#  define IMAGESHARER_EXPORT_DEPRECATED Q_DECL_DEPRECATED IMAGESHARER_EXPORT
 # endif
 
 #endif
