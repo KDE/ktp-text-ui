8d6f4b7f91ea66086a46a81c6111dab825b7fb63
Alexander Potashev <aspotashev@gmail.com>
2015-05-04 18:29:51 +0300

    Move config/* into subdirectories and fix untranslatable strings
    Fixed untranslatable strings by:
     1. Extracting strings from all *.cpp and *.ui files,
     2. Setting the correct translations domains for each KCM.
    
    BUG: 340804

--- config/Messages.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC appearance-config.ui >> rc.cpp || exit 11
-$XGETTEXT appearance-config.cpp rc.cpp -o $podir/kcm_ktp_chat_appearance.pot
-rm -f rc.cpp
-
-$EXTRACTRC behavior-config.ui >> rc.cpp || exit 11
-$XGETTEXT behavior-config.cpp rc.cpp -o $podir/kcm_ktp_chat_behavior.pot
-rm -f rc.cpp
---
+++ config/messages/Messages.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+$XGETTEXT `find . -name \*.cpp` -o $podir/kcm_ktp_chat_messages.pot
