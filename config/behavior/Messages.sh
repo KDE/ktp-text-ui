4ce1a597f25fa033f23672145d9bfafb4edf88ab
Alexander Potashev <aspotashev@gmail.com>
2015-05-05 17:05:50 +0300

    Fix POT name kcm_ktp_chat_behavior.pot

--- config/behavior/Messages.sh
+++ config/behavior/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp` -o $podir/kcm_ktp_chat_behaviour.pot
+$XGETTEXT `find . -name \*.cpp` -o $podir/kcm_ktp_chat_behavior.pot
 rm -f rc.cpp
