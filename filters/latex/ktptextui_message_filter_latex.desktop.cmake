32ccc099d5f3b89966a147f8f2c506dac8025e63
l10n daemon script <scripty@kde.org>
2017-01-03 12:35:10 +0100

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- filters/latex/ktptextui_message_filter_latex.desktop.cmake
+++ filters/latex/ktptextui_message_filter_latex.desktop.cmake
@@ -16,6 +16,7 @@ X-KTp-PluginInfo-Version=@KTP_MESSAGE_FILTER_FRAMEWORK_VERSION@
 
 Name=LaTeX Renderer
 Name[ar]=مصيّر لاتخ
+Name[ast]=Renderizador LaTeX
 Name[bs]=LaTeX Obrađivač
 Name[ca]=Renderitzador LaTeX
 Name[ca@valencia]=Renderitzador LaTeX
